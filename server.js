dbf697fa439aea4f4609232b713a904abe710565
truecoder34 <vladplotnikov34@gmail.com>
2017-11-24 01:38:55 +0300

    Version 1.0.2

--- server.js
+++ server.js
@@ -57,6 +57,11 @@ app.get("/so_reg", function(req, res) {
   res.sendFile(__dirname + "/registration_SO.html");
 })
 
+app.get("/coach_reg", function(req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.sendFile(__dirname + "/registration_coach.html");
+})
+
 // Вывести все спортивные организации
 app.get("/sport_org", function(req, res) {
    res.setHeader("Content-Type", "text/html");
@@ -64,15 +69,35 @@ app.get("/sport_org", function(req, res) {
      `quantity_of_sections` ,  `quantity_of_groups` ,  `financ_type` , connect_so_st.sport_type FROM sport_organization INNER \
     JOIN connect_so_st ON sport_organization.number_SO = connect_so_st.number_SO", function(err, result, fields) {
       if (err) {
-        console.log("sport organization table SELECT - ERROR");
+        console.log("ERROR: Notes from 'sport_organization' table  DOESN'T SELECTED - ERROR");
         answer = "ERROR";
         // res.send(answer);
       } else {
-        console.log("sport organization table SELECT - OK");
+        console.log("Success: Notes from 'sport_organization' table SELECTED - DONE");
         res.render('sport_org', {
           res:result
         })
-        console.log("sport organization table SEND ok");
+        console.log("Success: Notes from 'sport_organization' table SENT - DONE");
+        // res.send(answer); // отправляем все данные из таблицы
+      }
+    });
+})
+
+// Вывести всех тренеров
+app.get("/coaches", function(req, res) {
+   res.setHeader("Content-Type", "text/html");
+   con.query("SELECT  `number_of_SO`, `sport_spec`, `fio_of_coach`, `experience`, `title_of_coach`, `age_of_coach`, `quantity_of_years_in_sport`, sport_organization.name_SO, sport_organization.number_SO\
+     FROM `coaches` INNER JOIN sport_organization ON coaches.number_of_SO = sport_organization.number_SO;", function(err, result, fields) {
+      if (err) {
+        console.log("ERROR: Notes from 'coaches' table  DOESN'T SELECTED - ERROR");
+        answer = "ERROR";
+        // res.send(answer);
+      } else {
+        console.log("Success: Notes from 'coaches' table SELECTED - DONE");
+        res.render('coaches', {
+          res:result
+        })
+        console.log("Success: Notes from 'coaches' table SENT - DONE");
         // res.send(answer); // отправляем все данные из таблицы
       }
     });
@@ -87,13 +112,33 @@ app.post("/sand_info_about_SO", function(req, res) {
     req.body.input_adress + "\",\"" + req.body.input_quantity_of_mem + "\",\"" + req.body.input_quantity_of_sec + "\",\"" + req.body.input_quantity_of_sg + "\",\"" + req.body.input_fin_type + "\");",
     function(err, result) {
       if (err) throw err;
-      console.log("INSERT in sport_organization - DONE");
+      console.log("Success: New note INSERTED in 'sport_organization' table - DONE");
     });
+    con.query("INSERT INTO kind_of_sport (sport_type) VALUES (\"" + req.body.sport_type + "\");",
+      function(err, result) {
+        if (err) throw err;
+        console.log("Success: New note INSERTED in 'kind_of_sport' table - DONE");
+      });
     con.query("INSERT INTO connect_so_st (sport_type,number_SO) VALUES (\"" + req.body.sport_type + "\",\"" + req.body.input_number + "\");",
       function(err, result) {
         if (err) throw err;
-        console.log("INSERT in connect_so_st -  DONE");
+        console.log("Success: New note INSERTED in 'connect_so_st' table - DONE");
       });
+
+  res.setHeader("Content-Type", "text/html");
+  res.sendFile(__dirname + "/registration_SO.html");
+})
+
+app.post("/sand_info_about_coach", function(req, res) {
+  var fio = req.body.input_first_name +" "+ req.body.input_surname +" "+ req.body.input_second_name;
+  /*обращаемся к базе данных, посылая запрос на вставку записис в базу.
+  Сначала прописываем поля, куда вставить, потом непосредственно значения для вставки */
+  con.query("INSERT INTO coaches (`number_of_SO`, `sport_spec`, `fio_of_coach`, `experience`, `title_of_coach`, `age_of_coach`, `quantity_of_years_in_sport`) VALUES( \"" + req.body.input_number_so + "\",\"" + req.body.input_sport_type + "\",\"" +
+    fio + "\",\"" + req.body.input_exp + "\",\"" + req.body.input_rang + "\",\"" + req.body.input_age + "\",\"" + req.body.input_years + "\");",
+    function(err, result) {
+      if (err) throw err;
+      console.log("Success: New note INSERTED in 'coaches' table - DONE");
+    });
 
   res.setHeader("Content-Type", "text/html");
   res.sendFile(__dirname + "/registration_SO.html");
